62ebbef93705713a7fc3a06fb78eed6a1c9b4b78
Paulo Sergio <pauloserg861@gmail.com>
2023-02-26 15:59:51 -0300

    setting cors and enabling logger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import express, { Request, Response } from "express";
+import logger from "morgan";
 
 export const app = express();
+
+// watching logs requests
+app.use(logger("dev"));
+
+// enabling cors
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "*");
+	res.setHeader("Access-Control-Allow-Headers", "*");
+
+	if (req.method === "OPTIONS") {
+		res.status(204).json([]);
+	} else {
+		next();
+	}
+});
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("testando API");
